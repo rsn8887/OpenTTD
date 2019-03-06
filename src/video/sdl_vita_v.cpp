b95e62d4dfc47c443766919c7d98bd56bc958939
rsn8887 <rsn8887@users.noreply.github.com>
2019-03-05 23:15:07 -0600

    switch: reduce analog pointer speed slightly

--- src/video/sdl_vita_v.cpp
+++ src/video/sdl_vita_v.cpp
@@ -312,7 +312,7 @@ static void HandleAnalogSticks(void)
 	_hires_dx += left_x; // sub-pixel precision to allow slow mouse motion at speeds < 1 pixel/frame
 	_hires_dy += left_y;
 	
-	const int slowdown = 4096;
+	const int slowdown = 6144;
 	
 	if (_hires_dx != 0 || _hires_dy != 0) {
 		int xrel = _hires_dx / slowdown;
